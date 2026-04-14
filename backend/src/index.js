d72362583bf541bb737b7be5274cd76490806b8d
Akshay-Aute <akshay.aute.01@gmail.com>
2026-04-14 11:27:56 +0530

    feat: refactor frontend structure with routing and authentication
    - Added routing with react-router-dom for navigation between pages.
    - Implemented authentication check using Zustand for state management.
    - Created new pages: HomePage, LoginPage, SignupPage, ProfilePage, and SettingPage.
    - Introduced a Navbar component for consistent navigation.
    - Removed unused assets and CSS styles, transitioning to Tailwind CSS for styling.
    - Set up Axios instance for API calls with base URL configuration.
    - Updated package.json to include new dependencies for Tailwind CSS, Axios, and Zustand.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -12,6 +12,10 @@ const PORT = process.env.PORT;
 
 app.use(express.json()); // to extract JSON data form body;
 app.use(cookieParser());
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true
+}))
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRoutes); // http://localhost:3000/api/auth
 app.use("/api/message", messageRoutes); // http://localhost:3000/api/message
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,15 @@
 import express from "express";
-import router from "express";
+const router = express.Router();
 import { protectRoute } from "../middleware/auth.middleware.js";
-import {getUsersForSidebar,getMessages,sendMessage} from "../controllers/message.controller.js";
+import {
+  getUsersForSidebar,
+  getMessages,
+  sendMessage,
+} from "../controllers/message.controller.js";
 
-router.get("/users", protectRoute,getUsersForSidebar);
+router.get("/users", protectRoute, getUsersForSidebar);
 
-router.get("/:id",protectRoute,getMessages);
+router.get("/:id", protectRoute, getMessages);
 
-router.post("/send/:id",protectRoute,sendMessage);
+router.post("/send/:id", protectRoute, sendMessage);
 export default router;
