26bdf85ef1d4eaa3b8ffb8a037cc6d5bbbb37303
Akshay-Aute <akshay.aute.01@gmail.com>
2026-04-15 18:39:36 +0530

    feat: implement chat functionality with sidebar, message handling, and loading states

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,13 +12,15 @@ const PORT = process.env.PORT;
 
 app.use(express.json()); // to extract JSON data form body;
 app.use(cookieParser());
-app.use(cors({
-  origin:"http://localhost:5173",
-  credentials:true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRoutes); // http://localhost:3000/api/auth
-app.use("/api/message", messageRoutes); // http://localhost:3000/api/message
+app.use("/api/messages", messageRoutes); // http://localhost:3000/api/message
 
 app.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,39 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
+
+
+export const useChatStore = create((set) => ({
+    messages: [],
+    users: [],
+    selectedUser: null,
+    isUsersLoading: false,
+    isMessagesLoading: false,
+
+    getUsers: async () => {
+        set({ isUsersLoading: true });
+        try {
+            const res = await axiosInstance.get("/messages/users");
+            set({ users: res.data });
+
+        } catch (error) {
+            toast.error(error.response.data.message || "Failed to load users");
+        } finally {
+            set({ isUsersLoading: false });
+        }
+    },
+
+    getMessages: async (userId) => {
+        set({ isMessagesLoading: true, selectedUser: userId });
+        try {
+            const res = await axiosInstance.get(`/messages/${userId}`);
+            set({ messages: res.data });
+        } catch (error) {
+            toast.error(error.response.data.message || "Failed to load messages");
+        } finally {
+            set({ isMessagesLoading: false });
+        }
+    },
+    // TODO: Optimize this later 
+    setSelectedUser: (selectedUser) => set({ selectedUser}),
+}));
